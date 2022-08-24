baf59e88dea7b2a4cad1e9cfe54d17566c6b0c8e
KevMaillard <86302957+KevMaillard@users.noreply.github.com>
2022-08-24 02:37:12 +0200

    Ajoutez des fichiers projet.

---
+++ DustKiller/MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DustKiller
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public DirectoryInfo winTemp;
+        public DirectoryInfo appTemp;
+
+        public MainWindow()
+        {
+            Console.WriteLine("DustKiller Power ON");
+            InitializeComponent();
+            winTemp = new DirectoryInfo(@"C:\Windows\Temp");
+            appTemp = new DirectoryInfo(System.IO.Path.GetTempPath());
+        }
+
+        // Calcul poids d'un dossier
+        public long DirSize(DirectoryInfo dir)
+        {
+            return dir.GetFiles().Sum(file => file.Length) + dir.GetDirectories().Sum(di => DirSize(di));
+        }
+
+        public void ClearTempData(DirectoryInfo di)
+        {
+            foreach (FileInfo file in di.GetFiles())
+            {
+                try
+                {
+                    file.Delete();
+                    Console.WriteLine(file.FullName);
+                }
+                catch (Exception ex)
+                {
+                    continue;
+                }
+            }
+
+            foreach (DirectoryInfo dir in di.GetDirectories())
+            
+            {
+                try
+                {
+                    dir.Delete();
+                    Console.WriteLine(dir.FullName);
+                }
+                catch (Exception ex)
+                {
+                    continue;
+                }
+
+            }
+        }
+
+
+        private void Button_Analyser_Click(object sender, RoutedEventArgs e)
+        {
+            AnalyseFolders();
+        }
+
+        public void AnalyseFolders()
+        {
+            Console.WriteLine("Début de l'analyse...");
+            long totalSize = 0;
+
+            totalSize += DirSize(winTemp) / 1000000;
+            totalSize += DirSize(appTemp) / 1000000;
+
+            espace.Content = totalSize + " Mb";
+            date.Content = DateTime.Now.ToString("dd'-'MM'-'yyyy");
+        }
+
+    }
+}
